8e4fe0f077db5ce6f20acc848187a1ee1cf03405
Heitor Prezia <h.prezia@gmail.com>
2017-06-20 16:58:54 -0300

    Created mock record for user

---
+++ mock/user.js
@@ -0,0 +1,7 @@
+var users = [
+  { id: 1, email: "john@mail.com", password: "john123" },
+  { id: 2, email: "sarah@mail.com", password: "sarah123" },
+  { id: 3, email: "mail@mail.com", password: "test123" },
+];
+
+module.exports = users;
